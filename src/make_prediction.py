62bf06f3b2c8ecc26d6c88c89735c081be28b3f1
Garrett Hauser <garrett@debian-Garrett.computing.com>
2025-11-19 16:25:22 -0700

    update function documentation

--- src/make_prediction.py
+++ src/make_prediction.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 # make one step prediction
-def prediction_step(trajectoryHistory_current, featureVector_current, coefficient_values):
-    # documentation
+def prediction_step(trajectoryHistory_current,
+                    featureVector_current,
+                    coefficient_values):
+    # trajectoryHistory_current - numpy array of dimensions (system) dim by _
+    # featureVector_current - feature vector for trajectoryHistory_current
+    # coefficient_values - coefficient values calculated in regression step
 
     prediction = trajectoryHistory_current + coefficient_values @ featureVector_current
+    
     return prediction
 
